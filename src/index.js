414a2be917a1746fc950ce432231e0edc46d77a7
Fernanda Michetti <f.michetti@hotmail.com>
2022-01-09 19:57:11 -0300

    Props e primeira forma de acessa-las

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-
+const title = "A Amiga Genial";
+const author = "Elena Ferrante";
+const img =
+  "https://m.media-amazon.com/images/I/913D8hnF++L._AC_UL480_FMwebp_QL65_.jpg";
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job="developer" />
+      <Book title="ramdom title" number={22} />
     </section>
   );
 }
 
-const Book = () => {
-  const title = "A Amiga Genial";
-  const author = "Elena Ferrante";
+const Book = (props) => {
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/913D8hnF++L._AC_UL480_FMwebp_QL65_.jpg"
-        alt="Capa do Livro A amiga genial de Elena Ferrante"
-      />
+      <img src={img} alt="Capa do Livro A amiga genial de Elena Ferrante" />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
-      {/* <p></p> */}
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   );
 };
